dade88cb783cf7629c05a9c776a10b98850a4ee1
limhalim <gkfla668@naver.com>
2022-03-08 19:34:40 +0900

    크레인 인형 뽑기 게임

--- src/programmers/level1/크레인_인형_뽑기_게임.java
+++ src/programmers/level1/크레인_인형_뽑기_게임.java
@@ -1,7 +1,46 @@
 package programmers.level1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class 크레인_인형_뽑기_게임 {
     public static void main(String[] args) {
-        // solution();
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
+        solution(board, moves);
+    }
+
+    public static int solution(int[][] board, int[] moves) {
+        int answer = 0; // 터져서 사라진 인형의 개수 변수
+        List<Integer> basket = new ArrayList<>(); // 크레인으로 뽑은 인형을 담을 바구니 배열
+
+        for (int location : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][location - 1] != 0) { // board에 인형이 있으면
+                    if (basket.isEmpty()) { // 바구니가 비어있는 경우
+                        basket.add(board[i][location - 1]); // 바구니에 넣어줌
+                        board[i][location - 1] = 0; // board에서 뽑아온 인형 자리는 빈칸으로
+                    } else { // 바구니가 비어있지 않은 경우
+                        if (basket.get(basket.size() - 1) == board[i][location - 1]) { // 바구니 마지막 요소가 넣으려는 값이랑 일치하는 경우
+                            basket.remove(basket.size() - 1); // 바구니 마지막 요소 삭제
+                            board[i][location - 1] = 0; // board에서 뽑아온 인형자리 빈칸으로
+                            answer += 2;
+
+                        } else { // 바구니 마지막 요소가 넣으려는 값이랑 일치하지 않는 경우
+                            basket.add(board[i][location - 1]); // 바구니에 넣어줌
+                            board[i][location - 1] = 0; // board에서 뽑아온 인형 자리는 빈칸
+                        }
+                    }
+                    break;
+                } else {
+                    if (i == board.length - 1) // 0인데 마지막 요소 일때
+                        break;
+                }
+            }
+        }
+
+        System.out.println("<바구니> : " + basket);
+        System.out.println("터져서 사라진 인형의 개수 : " + answer);
+        return answer;
     }
 }
--- src/programmers/level1/키패드_누르기.java
+++ src/programmers/level1/키패드_누르기.java
@@ -1,7 +1,179 @@
 package programmers.level1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class 키패드_누르기 {
+    private static final String RIGHT_SYMBOL = "R";
+    private static final String LEFT_SYMBOL = "L";
+    private static final String LEFT_HAND = "left";
+    private static final String RIGHT_HAND = "right";
+
     public static void main(String[] args) {
-        // solution();
+        int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+        String hand = LEFT_HAND;
+
+        solution(numbers, hand);
+    }
+
+    public static String solution(int[] numbers, String hand) {
+        String usedHand = "";
+        String answer = "";
+
+        List<String> two = Arrays.asList("1", "3", "5");
+        List<String> five = Arrays.asList("2", "4", "6", "8");
+        List<String> eight = Arrays.asList("0", "5", "7", "9");
+        List<String> zero = Arrays.asList("8", "#", "*");
+
+        // 왼손, 오른손 거리 변수 생성
+        int leftDist = 0;
+        int rightDist = 0;
+
+        // 2는 리스트에 포함(거리 1) 안되면 4, 6, 8은 거리 2 / 7,0,9는 거리 3
+        // 5는 리스트에 포함(거리 1) 안되면 위치 다 똑같음 (o)
+        // 8은 리스트에 포함(거리 1) 안되면 2, 4, 6은 거리 2 / 1, 3은 거리 3
+        // 0은 리스트에 포함(거리 1) 안되면 5,7,9는 거리 2 / 2,4,6은 거리 3 / 1,3은 거리4
+
+        // 왼손, 오른손 위치 기억 변수 생성
+        String leftHand = "*";
+        String rightHand = "#";
+
+        for (int number : numbers) {
+            if (number == 1 || number == 4 || number == 7) { // 왼손이 클릭하는 경우
+                usedHand = LEFT_SYMBOL;
+
+            } else if (number == 3 || number == 6 || number == 9) { // 오른손이 클릭하는 경우
+                usedHand = RIGHT_SYMBOL;
+
+            } else if (number == 2) { // 눌러야 할 숫자가 2인 경우
+                if (two.contains(leftHand) && !two.contains(rightHand)) { // 왼손만 포함
+                    usedHand = LEFT_SYMBOL;
+
+                } else if (!two.contains(leftHand) && two.contains(rightHand)) { // 오른손만 포함
+                    usedHand = RIGHT_SYMBOL;
+
+                } else if (two.contains(leftHand) && two.contains(rightHand)) { // 둘다 포함 (거리 같음)
+                    if (hand.equals(RIGHT_HAND)) {
+                        usedHand = RIGHT_SYMBOL;
+                    } else {
+                        usedHand = LEFT_SYMBOL;
+                    }
+                } else { // 둘다 안 포함
+                    if (leftHand.equals("4") || leftHand.equals("6") || leftHand.equals("8")) { // 왼손 위치가 4, 6이라면
+                        leftDist = 2;
+                    } else {
+                        leftDist = 3;
+                    }
+
+                    if (rightHand.equals("4") || rightHand.equals("6") || rightHand.equals("8")) { // 오른손 위치가 4, 6이라면
+                        rightDist = 2;
+                    } else {
+                        rightDist = 3;
+                    }
+
+                    if (leftDist > rightDist) { // 오른손이 더 가까우면
+                        usedHand = RIGHT_SYMBOL;
+                    } else { // 왼손이 더 가까우면
+                        usedHand = LEFT_SYMBOL;
+                    }
+                }
+            } else if (number == 8) { // 눌러야 할 숫자가 8인 경우
+                if (eight.contains(leftHand) && !eight.contains(rightHand)) { // 왼손만 포함
+                    usedHand = LEFT_SYMBOL;
+
+                } else if (!eight.contains(leftHand) && eight.contains(rightHand)) { // 오른손만 포함
+                    usedHand = RIGHT_SYMBOL;
+
+                } else if (eight.contains(leftHand) && eight.contains(rightHand)) { // 둘다 포함 (거리 같음)
+                    if (hand.equals(RIGHT_HAND)) {
+                        usedHand = RIGHT_SYMBOL;
+                    } else {
+                        usedHand = LEFT_SYMBOL;
+                    }
+                } else { // 둘다 안 포함
+                    if (leftHand.equals("2") || leftHand.equals("4") || leftHand.equals("6")) { // 왼손 위치가 2, 4, 6이라면
+                        leftDist = 2;
+                    } else {
+                        leftDist = 3;
+                    }
+
+                    if (rightHand.equals("2") || rightHand.equals("4") || rightHand.equals("6")) { // 오른손 위치가 2, 4, 6이라면
+                        rightDist = 2;
+                    } else {
+                        rightDist = 3;
+                    }
+
+                    if (leftDist > rightDist) { // 오른손이 더 가까우면
+                        usedHand = RIGHT_SYMBOL;
+                    } else { // 왼손이 더 가까우면
+                        usedHand = LEFT_SYMBOL;
+                    }
+                }
+            } else if (number == 5) { // 눌러야 할 숫자가 5인 경우
+                if (five.contains(leftHand) && !five.contains(rightHand)) { // 왼손만 포함
+                    usedHand = LEFT_SYMBOL;
+
+                } else if (!five.contains(leftHand) && five.contains(rightHand)) { // 오른손만 포함
+                    usedHand = RIGHT_SYMBOL;
+
+                } else { // 왼손 오른손 둘다 리스트에 포함(거리 1)되거나 안되는 경우 (거리 2)
+                    if (hand.equals(RIGHT_HAND)) {
+                        usedHand = RIGHT_SYMBOL;
+                    } else {
+                        usedHand = LEFT_SYMBOL;
+                    }
+                }
+            } else if (number == 0) { // 눌러야 할 숫자가 0인 경우
+                if (zero.contains(leftHand) && !zero.contains(rightHand)) { // 왼손만 포함
+                    usedHand = LEFT_SYMBOL;
+
+                } else if (!zero.contains(leftHand) && zero.contains(rightHand)) { // 오른손만 포함
+                    usedHand = RIGHT_SYMBOL;
+
+                } else if (zero.contains(leftHand) && zero.contains(rightHand)) { // 둘다 포함 (거리 같음)
+                    if (hand.equals(RIGHT_HAND)) {
+                        usedHand = RIGHT_SYMBOL;
+                    } else {
+                        usedHand = LEFT_SYMBOL;
+                    }
+                } else { // 둘다 안 포함
+                    if (leftHand.equals("5") || leftHand.equals("7") || leftHand.equals("9")) { // 왼손 위치가 5, 7, 9라면
+                        leftDist = 2;
+                    } else if (leftHand.equals("2") || leftHand.equals("4") || leftHand.equals("6")) { // 왼손 위치가 2, 4, 6이라면
+                        leftDist = 3;
+                    } else {
+                        leftDist = 4;
+                    }
+
+                    if (rightHand.equals("5") || rightHand.equals("7") || rightHand.equals("9")) { // 오른손 위치가 5, 7, 9라면
+                        rightDist = 2;
+                    } else if (rightHand.equals("2") || rightHand.equals("4") || rightHand.equals("6")) { // 오른손 위치가 2, 4, 6이라면
+                        rightDist = 3;
+                    } else {
+                        rightDist = 4;
+                    }
+
+                    if (leftDist > rightDist) { // 오른손이 더 가까우면
+                        usedHand = RIGHT_SYMBOL;
+                    } else { // 왼손이 더 가까우면
+                        usedHand = LEFT_SYMBOL;
+                    }
+                }
+            }
+            if (usedHand == LEFT_SYMBOL) {
+                leftHand = Integer.toString(number);
+            } else {
+                rightHand = Integer.toString(number);
+            }
+
+            answer += usedHand;
+            /*System.out.println("왼손위치 : " + leftHand);
+            System.out.println("오른손위치 : " + rightHand);
+            System.out.println("눌러야 할 숫자 : " + numbers[i]);
+            System.out.println("사용한 손: " + usedHand);
+            System.out.println("");*/
+        }
+        System.out.println(answer);
+        return answer;
     }
 }
