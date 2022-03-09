e8f074e3405227087fded11a1e799fc0444ebf1e
limhalim <gkfla668@naver.com>
2022-03-09 18:18:16 +0900

    내적

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 * [음양 더하기](https://programmers.co.kr/learn/courses/30/lessons/76501)
 * [없는 숫자 더하기](https://programmers.co.kr/learn/courses/30/lessons/86051)
 * [완주하지 못한 선수](https://programmers.co.kr/learn/courses/30/lessons/42576)
+* [내적](https://programmers.co.kr/learn/courses/30/lessons/70128)
--- src/programmers/level1/내적.java
+++ src/programmers/level1/내적.java
@@ -2,6 +2,16 @@ package programmers.level1;
 
 public class 내적 {
     public static void main(String[] args) {
-        // solution();
+        int[] a = {-1,0,1};
+        int[] b = {1,0,-1};
+        solution(a, b);
+    }
+
+    public static int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++)
+            answer += a[i]*b[i];
+
+        return answer;
     }
 }
