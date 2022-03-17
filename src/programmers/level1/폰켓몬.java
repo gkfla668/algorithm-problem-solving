f03ee63a67a4bd089ab2c05f1ba630c6e1194271
limhalim <gkfla668@naver.com>
2022-03-18 00:36:03 +0900

    폰켓몬

--- src/programmers/level1/폰켓몬.java
+++ src/programmers/level1/폰켓몬.java
@@ -1,7 +1,18 @@
 package programmers.level1;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class 폰켓몬 {
     public static void main(String[] args) {
-        // solution();
+        int[] nums = {3, 1, 2, 3};
+        solution(nums);
+    }
+
+    public static int solution(int[] nums) {
+        Set<Integer> numbers = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+        return numbers.size() > (nums.length / 2) ? nums.length / 2 : numbers.size();
     }
 }
