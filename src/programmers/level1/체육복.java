be8b044912051511a251cfeb4d8f5ccb358d7b1d
limhalim <gkfla668@naver.com>
2022-03-17 23:56:33 +0900

    체육복

--- src/programmers/level1/체육복.java
+++ src/programmers/level1/체육복.java
@@ -1,7 +1,38 @@
 package programmers.level1;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class 체육복 {
     public static void main(String[] args) {
-        // solution();
+        int n = 5;
+        int[] lost = {2, 4};
+        int[] reserve = {3, 1};
+        solution(n, lost, reserve);
+    }
+
+    public static int solution(int n, int[] lost, int[] reserve) {
+        List<Integer> lostStd = Arrays.stream(lost).boxed().collect(Collectors.toList());
+        List<Integer> reserveStd = Arrays.stream(reserve).boxed().collect(Collectors.toList());
+
+        for (int i : reserve) {
+            if (lostStd.contains(i)) { // 여벌 체육복 도난
+                lostStd.remove(Integer.valueOf(i));
+                reserveStd.remove(Integer.valueOf(i));
+            }
+        }
+
+        Collections.sort(reserveStd);
+
+        for (int i : reserveStd) {
+            if (lostStd.contains(i - 1))
+                lostStd.remove(Integer.valueOf(i - 1));
+            else if (lostStd.contains(i + 1))
+                lostStd.remove(Integer.valueOf(i + 1));
+        }
+
+        return n - lostStd.size();
     }
 }
