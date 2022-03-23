a1c88d7b2000aa7b383f77abca04d0058f3a206b
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-03-24 05:19:17 +0900

    두 정수 사이의 합

--- src/programmers/level1/두_정수_사이의_합.java
+++ src/programmers/level1/두_정수_사이의_합.java
@@ -2,6 +2,34 @@ package programmers.level1;
 
 public class 두_정수_사이의_합 {
     public static void main(String[] args) {
-        // solution();
+        int a = 5;
+        int b = 3;
+        solution(a, b);
+    }
+
+    public static long solution(int a, int b) {
+        /* 등차수열 공식
+        public long solution(int a, int b) {
+            return sumAtoB(Math.min(a, b), Math.max(b, a));
+        }
+
+        private long sumAtoB(long a, long b) {
+            return (b - a + 1) * (a + b) / 2;
+        }
+        */
+
+        long answer = 0;
+        int temp;
+
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        for (int i = a; i <= b; i++)
+            answer += i;
+
+        return answer;
     }
 }
