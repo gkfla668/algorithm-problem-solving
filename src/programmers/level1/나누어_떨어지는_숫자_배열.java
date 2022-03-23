5eb19521404cb905d3eb81574149c36bba0625db
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-03-24 05:00:50 +0900

    나누어 떨어지는 숫자 배열

--- src/programmers/level1/나누어_떨어지는_숫자_배열.java
+++ src/programmers/level1/나누어_떨어지는_숫자_배열.java
@@ -1,7 +1,34 @@
 package programmers.level1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class 나누어_떨어지는_숫자_배열 {
     public static void main(String[] args) {
-        // solution();
+        int[] arr = {2,36,1,3};
+        int divisor = 1;
+        solution(arr, divisor);
+    }
+
+    public static int[] solution(int[] arr, int divisor) {
+        /* Arrays.stream(arr).filter(f -> f % divisor == 0).toArray(); */
+
+        List<Integer> list = new ArrayList<>();
+
+        for (int num : arr) {
+            if (num % divisor == 0)
+                list.add(num);
+        }
+
+        if (list.size() == 0)
+            list.add(-1);
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++)
+            answer[i] = list.get(i);
+
+        Arrays.sort(answer);
+        return answer;
     }
 }
