d2083f9c188da088cae967d2bdf73543838b921f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-03-24 18:08:34 +0900

    서울에서 김서방 찾기

--- src/programmers/level1/서울에서_김서방_찾기.java
+++ src/programmers/level1/서울에서_김서방_찾기.java
@@ -1,4 +1,20 @@
 package programmers.level1;
 
 public class 서울에서_김서방_찾기 {
+    public static void main(String[] args) {
+        String[] seoul = {"Jane", "Kim"};
+        solution(seoul);
+    }
+
+    public static String solution(String[] seoul) {
+        int idx = 0;
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                idx = i;
+                break;
+            }
+        }
+
+        return "김서방은 " + idx + "에 있다";
+    }
 }
