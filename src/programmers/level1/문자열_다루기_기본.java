ab093b18603da980e36ff1f747a01c52490b8b5d
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-03-24 17:27:36 +0900

    문자열 다루기 기본

--- src/programmers/level1/문자열_다루기_기본.java
+++ src/programmers/level1/문자열_다루기_기본.java
@@ -2,6 +2,21 @@ package programmers.level1;
 
 public class 문자열_다루기_기본 {
     public static void main(String[] args) {
-        // solution();
+        String s = "1234";
+        solution(s);
+    }
+
+    public static boolean solution(String s) {
+        boolean isDigit = true;
+
+        char[] string = s.toCharArray();
+        for(char ch : string){
+            if(!Character.isDigit(ch)) {
+                isDigit = false;
+                break;
+            }
+        }
+
+        return (s.length() == 4 || s.length() == 6) && isDigit;
     }
 }
