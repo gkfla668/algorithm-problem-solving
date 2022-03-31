d8e0f7c87d0f9018ee3ed4b3456222838fa62651
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-01 00:55:52 +0900

    자연수 뒤집어 배열로 만들기

--- src/programmers/level1/자연수_뒤집어_배열로_만들기.java
+++ src/programmers/level1/자연수_뒤집어_배열로_만들기.java
@@ -1,4 +1,27 @@
 package programmers.level1;
 
+import java.util.Arrays;
+
 public class 자연수_뒤집어_배열로_만들기 {
+    public static void main(String[] args) {
+        long n = 5;
+        solution(n);
+    }
+
+    public static int[] solution(long n) {
+        /* 형변환 주의 */
+        /* long type 길이 구하는 법
+        * String a = "" + n;
+        * a.length(); */
+
+        int length = (int) (Math.log10(n) + 1);
+        int[] answer = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            answer[i] = (int) (n % 10);
+            n /= 10;
+        }
+
+        return answer;
+    }
 }
