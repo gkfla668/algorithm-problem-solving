2a604fb3c4f1175453432e615ff066082132b7b3
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-01 00:06:38 +0900

    자릿수 더하기

--- src/programmers/level1/자릿수_더하기.java
+++ src/programmers/level1/자릿수_더하기.java
@@ -1,4 +1,26 @@
 package programmers.level1;
 
 public class 자릿수_더하기 {
+    public static void main(String[] args) {
+        int n = 987;
+        solution(n);
+    }
+
+    public static int solution(int n) {
+        /*
+        while(true){
+            answer += n % 10;
+            if(n < 10)
+                break;
+
+            n = n / 10;
+        }*/
+
+        int answer = 0;
+        String str = String.valueOf(n);
+        for (int i = 0; i < str.length(); i++)
+            answer += Integer.parseInt(str.substring(i, i + 1));
+
+        return answer;
+    }
 }
