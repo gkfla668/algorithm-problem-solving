e22c8142f6bdfc31824a133ec1faed377cee572b
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-01 01:46:24 +0900

    제일 작은 수 제거하기

--- src/programmers/level1/제일_작은_수_제거하기.java
+++ src/programmers/level1/제일_작은_수_제거하기.java
@@ -1,7 +1,19 @@
 package programmers.level1;
 
+import java.util.Arrays;
+
 public class 제일_작은_수_제거하기 {
     public static void main(String[] args) {
-        // solution();
+        int[] arr = {4};
+        solution(arr);
+    }
+
+    public static int[] solution(int[] arr) {
+        int min = Arrays.stream(arr).min().getAsInt();
+        int[] answer = Arrays.stream(arr).filter(num -> num != min).toArray();
+        if(answer.length == 0)
+            answer = new int[] {-1};
+
+        return answer;
     }
 }
