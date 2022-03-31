ceef71c6c107edb53f621c1987cd77dbd597e19a
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-01 02:20:59 +0900

    하샤드 수

--- src/programmers/level1/하샤드_수.java
+++ src/programmers/level1/하샤드_수.java
@@ -2,6 +2,19 @@ package programmers.level1;
 
 public class 하샤드_수 {
     public static void main(String[] args) {
-        // solution();
+        int x = 10;
+        solution(x);
+    }
+
+    public static boolean solution(int x) {
+        int sum = 0;
+        String[] str = String.valueOf(x).split("");
+        for(String num : str)
+            sum += Integer.parseInt(num);
+
+        if(x % sum == 0)
+            return true;
+        else
+            return false;
     }
 }
