e26e83580dde3f4d93c11ad85aaf55d1654980bb
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-04-01 03:17:35 +0900

    핸드폰 번호 가리기

--- src/programmers/level1/핸드폰_번호_가리기.java
+++ src/programmers/level1/핸드폰_번호_가리기.java
@@ -2,6 +2,25 @@ package programmers.level1;
 
 public class 핸드폰_번호_가리기 {
     public static void main(String[] args) {
-        // solution();
+        String phone_number = "027778888";
+        solution(phone_number);
+    }
+
+    public static String solution(String phone_number) {
+        String secret = "";
+        String str = phone_number.substring(phone_number.length() - 4);
+
+        for (int i = 0; i < phone_number.length() - 4; i++)
+            secret += "*";
+
+        return secret + str;
+
+        /* 다른 사람 풀이
+        *
+        char[] ch = phone_number.toCharArray();
+        for(int i = 0; i < ch.length - 4; i ++){
+            ch[i] = '*';
+        }
+        return String.valueOf(ch);*/
     }
 }
