5ae2abc54c35224c185e56a286b0cc5fb4d32b05
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-06-07 21:27:43 +0900

    소수 만들기

--- README.md
+++ README.md
@@ -43,10 +43,10 @@
 * [소수 찾기](https://programmers.co.kr/learn/courses/30/lessons/12921)
 * [이상한 문자 만들기](https://programmers.co.kr/learn/courses/30/lessons/12930)
 * [시저 암호](https://programmers.co.kr/learn/courses/30/lessons/12926)
+* [소수 만들기](https://programmers.co.kr/learn/courses/30/lessons/12977)
 
 ##### <보류>
 * [키패드 누르기](https://programmers.co.kr/learn/courses/30/lessons/67256) `완전 탐색`
-* [소수 만들기](https://programmers.co.kr/learn/courses/30/lessons/12977)
 * [2016년](https://programmers.co.kr/learn/courses/30/lessons/12901)
 * [1차 다트게임](https://programmers.co.kr/learn/courses/30/lessons/17682)
 * [3진법 뒤집기](https://programmers.co.kr/learn/courses/30/lessons/68935)
--- src/programmers/level1/소수_만들기.java
+++ src/programmers/level1/소수_만들기.java
@@ -2,6 +2,36 @@ package programmers.level1;
 
 public class 소수_만들기 {
     public static void main(String[] args) {
-        // solution();
+        int[] nums = {1, 2, 7, 6, 4};
+        solution(nums);
+    }
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (prime(sum))
+                        answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private static boolean prime(int num) {
+        boolean isPrime = true;
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        return isPrime;
     }
 }
