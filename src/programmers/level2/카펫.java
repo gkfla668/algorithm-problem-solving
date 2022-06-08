c9663e6799f9875ff737d2d039239782ad7858e5
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-06-08 18:11:32 +0900

    카펫

--- src/programmers/level2/카펫.java
+++ src/programmers/level2/카펫.java
@@ -11,7 +11,7 @@ public class 카펫 {
         int[] answer = new int[2];
         int num = brown + yellow;
 
-        for (int i = 3; i < num; i++) { // 가운데 yellow 이려면 가로세로 3이상이여야 함
+        for (int i = 3; i < num / 2; i++) { // 가운데 yellow 이려면 가로세로 3이상이여야 함 , 약수는 num/2보다 클 수 없음
             if (num % i == 0 && ((i - 2) * ((num / i) - 2) == yellow)) { // 약수면서
                 answer[0] = num / i;
                 answer[1] = i;
