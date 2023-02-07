7b9f3ace3b9cf99c313c8ad72a3c7d20301e68dd
Halim-Lim <gkfla668@naver.com>
2023-02-07 20:52:16 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,21 +1,17 @@
 function solution(progresses, speeds) {
-    let successDays = progresses.map((num, idx) => {
-        return Math.ceil((100 - num) / speeds[idx]);
-    })
+    let successDays = progresses.map((num, idx) => Math.ceil((100 - num) / speeds[idx]))
+    let answer = new Array(successDays.length).fill(0);
+    let day = 0;
+    answer[day] = 1;
     
-    let answer = new Array(Math.max(...successDays) + 1).fill(0);
-    
-    for(let i = 0; i < successDays.length; i++){
-        let day = successDays[i];
-        
-        if(i > 0 && successDays[i] < successDays[i - 1]){
-            day = successDays[i - 1];
+    for(let i = 1; i < successDays.length; i++){
+        if(successDays[i] <= successDays[i - 1]){
             answer[day]++;
             successDays.splice(i, 1);
             i--;
         }else
-            answer[day]++;
+            answer[++day]++;
     }
-    
-    return answer.filter((num) => num !== 0);
-}
+
+    return answer.filter((cnt) => cnt !== 0);
+    }
