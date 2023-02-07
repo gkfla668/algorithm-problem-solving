06435d54cff9c281225970ced6ef12a2ff3f1855
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-02-07 21:23:06 +0900

    Update README.md

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,17 +1,20 @@
 function solution(progresses, speeds) {
-    let successDays = progresses.map((num, idx) => Math.ceil((100 - num) / speeds[idx]))
-    let answer = new Array(successDays.length).fill(0);
-    let day = 0;
-    answer[day] = 1;
-    
-    for(let i = 1; i < successDays.length; i++){
-        if(successDays[i] <= successDays[i - 1]){
-            answer[day]++;
-            successDays.splice(i, 1);
-            i--;
-        }else
-            answer[++day]++;
+  let successDays = progresses.map((num, idx) =>
+    Math.ceil((100 - num) / speeds[idx])
+  );
+  let answer = new Array(successDays.length).fill(0);
+  let day = 0;
+  answer[day] = 1;
+
+  for (let i = 1; i < successDays.length; i++) {
+    if (deployDays[i - 1] >= deployDays[i]) {
+      day--;
+      deployDays.splice(i, 1);
+      i = day + 1;
     }
 
-    return answer.filter((cnt) => cnt !== 0);
-    }
+    answer[++day]++;
+  }
+
+  return answer.filter((cnt) => cnt !== 0);
+}
