925e841a6f65fcc2586d2ede1d42b25ffafd6387
Halim-Lim <gkfla668@naver.com>
2023-02-12 00:19:08 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.79 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,12 +1,6 @@
 function solution(A,B){
-    const size = A.length;
-    var answer = 0;
-    
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
 
-    for(let i = 0; i < size; i++)
-        answer += A.shift() * B.shift();
-
-    return answer;
+    return A.reduce((acc, cur, idx) => {return acc += cur * B[idx]}, 0);
 }
