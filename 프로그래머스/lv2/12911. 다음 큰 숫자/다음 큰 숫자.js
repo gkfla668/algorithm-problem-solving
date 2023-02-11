ca9472974e24549757937797e2530fc1a9d7640d
Halim-Lim <gkfla668@naver.com>
2023-02-12 01:54:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,11 +1,8 @@
 function solution(n) {
-    let target = n + 1;
-    let oneCnt1 = (n.toString(2).match(/1/g) || []).length;
+    let oneCnt = (n.toString(2).match(/1/g) || []).length;
     
-    while(true){
-        if(oneCnt1 === (target.toString(2).match(/1/g) || []).length)
-            return target;
-    
-        target++;
+    while(n++){
+        if(oneCnt === (n.toString(2).match(/1/g) || []).length)
+            return n;
     }
 }
