9eb508aabaa09409d93dd5e18d62d27add90c7a8
Halim-Lim <gkfla668@naver.com>
2023-02-12 16:12:22 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,13 @@
+function solution(n, words) {
+    let obj = {};
+    obj[words[0]] = true;
+    
+    for(let i = 1; i < words.length; i++){
+        if(obj[words[i]] || words[i - 1].substr(-1) !== words[i].charAt(0))
+            return [Math.trunc((i % n) + 1), Math.trunc((i / n) + 1)]
+        
+        obj[words[i]] = true;
+    }
+
+    return [0, 0];
+}
