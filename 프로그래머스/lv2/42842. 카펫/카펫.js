503c00ab043228fd763b2f76a5d8cce26870d3d3
Halim-Lim <gkfla668@naver.com>
2023-02-12 17:14:11 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -2,18 +2,19 @@ function solution(brown, yellow) {
     let num = brown + yellow;
     let col, row;
     
-    const divisors = [];
-    for(let i = 1 ; i <= num/2 ; i++){
-        if(num % i === 0) divisors.push(i);
-    }
-    
-    for(let i = 0; i < divisors.length; i++){
-        col = divisors[i];
-        for(let j = i; j < divisors.length; j++){
-            row = divisors[j];
-            
-            if(((row - 2) * (col - 2)) === yellow && row * col === num)
-                return [row, col];
+    for(let i = 1; i <= num/2; i++){
+        if(num % i === 0){
+            col = i;
+            row = col;
+
+            for(let j = i; j <= num/2; j++){
+                if(num % j === 0){
+                    row = j;
+
+                    if(((row - 2) * (col - 2)) === yellow && row * col === num)
+                        return [row, col];
+                }
+            }
         }
     }    
 }
