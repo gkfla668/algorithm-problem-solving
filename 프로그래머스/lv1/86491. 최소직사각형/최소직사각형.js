5e030577380c0cadcfb48eb1132fff3381d2b5cb
Halim-Lim <gkfla668@naver.com>
2023-02-15 21:07:37 +0900

    [level 1] Title: 최소직사각형, Time: 4.94 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,8 +1,8 @@
 function solution(sizes) {
     let maxSize = [0, 0];
-    let map = sizes.map((size) => size[0] < size[1] ? [size[1], size[0]] : [size[0], size[1]])
+    sizes = sizes.map((size) => size[0] < size[1] ? [size[1], size[0]] : [size[0], size[1]])
     
-    map.forEach((size) => {
+    sizes.forEach((size) => {
         if(maxSize[0] < size[0]) maxSize[0] = size[0];
         if(maxSize[1] < size[1]) maxSize[1] = size[1];
     })
