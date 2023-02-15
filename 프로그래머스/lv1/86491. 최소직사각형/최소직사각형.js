9325b43a22110c83051760933b331c0fbf3e4565
Halim-Lim <gkfla668@naver.com>
2023-02-15 21:18:13 +0900

    [level 1] Title: 최소직사각형, Time: 2.19 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -9,3 +9,22 @@ function solution(sizes) {
     
     return maxSize[0] * maxSize[1];
 }
+
+// function solution(sizes) {
+//     var answer = 0;
+//     let row = [];
+//     let col = [];
+    
+//     sizes.forEach((size) => {
+//         if(size[0] <= size[1]){
+//             col.push(size[1])
+//             row.push(size[0]);
+//         }else {
+//             row.push(size[1])
+//             col.push(size[0])
+//         }
+        
+//     })
+    
+//     return Math.max(...row) * Math.max(...col);
+// }
