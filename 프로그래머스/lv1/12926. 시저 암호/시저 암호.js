281dab0d0d083e5e46c5d19cc7973308adcc7e2f
Halim-Lim <gkfla668@naver.com>
2023-02-16 18:32:07 +0900

    [level 1] Title: 시저 암호, Time: 1.84 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,17 @@
+function solution(s, n) {
+    return [...s].map((it) => {
+        if(it === ' ')
+            return it;
+        
+        if(it.charCodeAt(0) <= 90){ // 대문자인 경우
+            if(it.charCodeAt(0) + n > 90)
+                return String.fromCharCode(64 + it.charCodeAt(0) + n - 90);
+        }
+        
+        // 소문자인 경우
+        if(it.charCodeAt(0) + n > 122)
+            return String.fromCharCode(96 + it.charCodeAt(0) + n - 122);
+        
+        return String.fromCharCode(it.charCodeAt(0) + n)
+    }).join("");
+}
