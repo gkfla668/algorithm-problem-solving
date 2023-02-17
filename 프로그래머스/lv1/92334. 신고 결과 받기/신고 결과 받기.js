04c158a274f00328c7546f01a314603725367b75
Halim-Lim <gkfla668@naver.com>
2023-02-18 05:15:41 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,31 +1,18 @@
 function solution(id_list, report, k) {
-    report = new Set(report); // 동일한 유저에 대한 신고 횟수는 1회로 처리
-
-    let reporter = new Map();
-    let reportedCnt = new Map();
-    report.forEach((it) => {
-        let [ reporter_id, reported_id ] = it.split(' ');
-        
-        reporter.set(reporter_id, (reporter.get(reporter_id) || '') + ' ' + reported_id);
-        reportedCnt.set(reported_id, (reportedCnt.get(reported_id) || 0) + 1); // 신고된 횟수
+    let reports = [...new Set(report)].map((it) => {
+        return it.split(' ')
+    }); // 동일한 유저에 대한 신고 횟수는 1회로 처리
+    
+    let reportedCnts = new Map();
+    reports.forEach((report) => {
+        reportedCnts.set(report[1], (reportedCnts.get(report[1]) || 0) + 1); // 신고된 횟수
     })
     
-    // k번 이상 신고된 유저는 게시판 이용이 정지
-    let stop_id = [];
-    reportedCnt.forEach((val, key) => {
-        if(val >= k) stop_id.push(key);
+    let mailCnts = new Map();
+    reports.forEach((report) => {
+        if(reportedCnts.get(report[1]) >= k)
+            mailCnts.set(report[0], (mailCnts.get(report[0]) || 0) + 1);
     })
     
-    reporter.forEach((val, key) => {
-        let ids = val.split(' ');
-        
-        let mailCnt = 0; //
-        ids.forEach((id) => {
-          if(stop_id.includes(id)) mailCnt++; // 신고한 유저가 정지되었다면  
-        })
-        
-        reporter.set(key, mailCnt);
-    })
-    
-    return id_list.map((id) => reporter.get(id) ? reporter.get(id) : 0);
+    return id_list.map((id) => mailCnts.get(id) ? mailCnts.get(id) : 0);
 }
