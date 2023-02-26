317c60b49d70744c2db875aa5e1480edda777549
Halim-Lim <gkfla668@naver.com>
2023-02-26 19:10:44 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,55 +1,38 @@
 function solution(str1, str2) {
-    let str1Arr = [];
-    let str2Arr = [];
+    let str1Set = [];
+    let str2Set = [];
     let pattern = /[a-zA-Z]/; // 영문자
     
-    for(let i = 0; i < str1.length; i++)
-        str1Arr.push(str1.slice(i, i + 2))
-    
-    str1Arr = str1Arr.filter((str) => {
-        let check = true;
-        for(let i = 0; i < 2; i++)
-            if(!pattern.test(str.charAt(i))) check = false; 
-    
-        return check;
-    })
-    
-    for(let i = 0; i < str2.length; i++)
-        str2Arr.push(str2.slice(i, i + 2))
-    
-    str2Arr = str2Arr.filter((str) => {
-        let check = true;
-        for(let i = 0; i < 2; i++)
-            if(!pattern.test(str.charAt(i))) check = false; 
+    for(let i = 0; i < str1.length - 1; i++){
+        if(!pattern.test(str1.charAt(i)) || !pattern.test(str1.charAt(i + 1))) continue;
+        str1Set.push(str1.slice(i, i + 2))
+    }
         
-        return check;
-    })
+     for(let i = 0; i < str2.length - 1; i++){ 
+         if(!pattern.test(str2.charAt(i)) || !pattern.test(str2.charAt(i + 1))) continue;
+        str2Set.push(str2.slice(i, i + 2))
+    }
     
-    if(str1Arr.length === 0 && str2Arr.length === 0)
+    if(str1Set.length === 0 && str2Set.length === 0)
         return 65536;
     
-    let intersection = []; // 교집합
-    let union = []; // 합집합
-    
     let map = new Map();
-    str2Arr.forEach((it) => {
-        map.set(it.toUpperCase(), (map.get(it.toUpperCase()) || 0) + 1);
+    str2Set.forEach((str) => {
+        map.set(str.toUpperCase(), (map.get(str.toUpperCase()) || 0) + 1);
     })
     
-    str1Arr.forEach((it) => {
-        if(map.get(it.toUpperCase()) && map.get(it.toUpperCase()) >= 1){
-            intersection.push(it);
-            map.set(it.toUpperCase(), map.get(it.toUpperCase()) - 1);
-        }else
-            union.push(it);
-    })
-
-    map.forEach((val, key) => {
-        while(val > 0){
-            union.push(key);
-            val--;
+    let intersectionLength = str1Set.filter((str) => {
+        if(map.get(str.toUpperCase()) >= 1){
+            map.set(str.toUpperCase(), map.get(str.toUpperCase()) - 1);
+            return true;
         }
+    }).length;
+    
+    let unionLength = str1Set.length - intersectionLength;
+    
+    map.forEach((val, key) => {
+        if(val > 0) unionLength += val;
     })
     
-    return Math.trunc((intersection.length / (union.length + intersection.length) * 65536));
+    return Math.trunc((intersectionLength / (unionLength + intersectionLength)) * 65536);
 }
