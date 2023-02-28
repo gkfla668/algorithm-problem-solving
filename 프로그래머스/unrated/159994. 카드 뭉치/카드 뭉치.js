db060b6670e6f017d8d7d31a80f20a784afebb7f
Halim-Lim <gkfla668@naver.com>
2023-02-28 16:34:00 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,8 +1,8 @@
 function solution(cards1, cards2, goal) {
-    for(let i = 0; i < goal.length; i++){
-        if(cards1[0] === goal[i]){
+    for(let str of goal){
+        if(cards1[0] === str){
             cards1.shift()
-        } else if(cards2[0] === goal[i]){
+        } else if(cards2[0] === str){
             cards2.shift()
         } else return 'No'
     }
