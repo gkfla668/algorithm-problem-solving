914e52683b676a4dc93a84b3877590455b80d290
Halim-Lim <gkfla668@naver.com>
2023-02-28 15:01:27 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -2,10 +2,7 @@ function solution(array, commands) {
     var answer = [];
     
     commands.forEach((val) => {
-        let i = val[0];
-        let j = val[1];
-        let k = val[2];
-        
+        let [i, j, k] = val;
         answer.push(array.slice(i - 1, j).sort((a, b) => a - b).find((val, index) => index === k - 1))
     })
     
