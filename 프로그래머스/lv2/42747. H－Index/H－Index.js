5c361cb4deec6067c84221e40f4f0c13a5ff0236
Halim-Lim <gkfla668@naver.com>
2023-03-01 04:59:41 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,3 +1,9 @@
 function solution(citations) {
+//     let  idx = 0;
+//     citations = citations.sort((a, b) => b - a)
+//     while(idx + 1 <= citations[idx]) idx++;
+    
+//     return idx;
+
     return citations.sort((a, b) => b - a).filter((el, idx) => el >= idx + 1).length;
 }
