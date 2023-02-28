026968e3cd252e62534d60c3c3eb2b5833631033
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-03-01 05:03:32 +0900

    Update README.md

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -3,7 +3,7 @@ function solution(array, commands) {
     
     commands.forEach((val) => {
         let [i, j, k] = val;
-        answer.push(array.slice(i - 1, j).sort((a, b) => a - b).find((val, index) => index === k - 1))
+        answer.push(array.slice(i - 1, j).sort((a, b) => a - b).find((val, index) => index === k - 1));
     })
     
     return answer;
--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,11 +1,11 @@
 function solution(cards1, cards2, goal) {
     for(let str of goal){
         if(cards1[0] === str){
-            cards1.shift()
+            cards1.shift();
         } else if(cards2[0] === str){
-            cards2.shift()
-        } else return 'No'
+            cards2.shift();
+        } else return 'No';
     }
     
-    return 'Yes'
+    return 'Yes';
 }
