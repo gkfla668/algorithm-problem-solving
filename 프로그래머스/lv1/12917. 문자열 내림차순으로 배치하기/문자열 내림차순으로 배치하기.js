050ad3d5038def6dcb7db87dc6b77f530df372ee
Halim-Lim <gkfla668@naver.com>
2023-03-03 21:21:37 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,7 +1,3 @@
 function solution(s) {
-    return [...s].sort((a, b) => {
-        if(a > b)
-            return -1;
-        return b - a;
-    }).join('');
+    return [...s].sort().reverse().join('');
 }
