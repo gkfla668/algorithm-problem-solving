dd1b5419f932d43854324aabf769b6eadb8795ba
Halim-Lim <gkfla668@naver.com>
2023-03-29 20:37:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    let dp = Array.from({length: 2001}, () => 0)
-    dp[1] = 1;
-    dp[2] = 2;
+    let dp = [0, 1, 2];
     
-    for(let i = 3; i <= 2000; i++){
+    for(let i = 3; i <= n; i++){
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
     }
 
