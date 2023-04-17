dacae3f7d13c2f25bfe1318afb4e088e7d6005a4
Halim-Lim <gkfla668@naver.com>
2023-04-17 23:36:02 +0900

    [unrated] Title: 추억 점수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -1,14 +1,9 @@
 function solution(name, yearning, photo) {
-    let answer = [];
-    
-    photo.forEach((picture) => {
+    return photo.map((picture) => {
         let score = 0;
-        picture.forEach((friend) => {
-            let s = name.indexOf(friend);
-            score += s !== -1 ? yearning[name.indexOf(friend)] : 0;
+        picture.map((friend) => {
+            score += name.indexOf(friend) !== -1 ? yearning[name.indexOf(friend)] : 0;
         })
-        answer.push(score)
+        return score;
     })
-    
-    return answer;
 }
