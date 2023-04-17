3d79a4607cf340888993bcd000d80c8e820f7a3c
Halim-Lim <gkfla668@naver.com>
2023-04-17 23:08:58 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -0,0 +1,22 @@
+function solution(k, score) {
+    let answer = [];
+    let queue = [];
+    
+    score.forEach((s) => {
+        if(queue.length < k){
+            queue.push(s);
+        } else {
+            if(queue[k-1] < s){
+                queue.pop();
+                queue.push(s);
+            }
+        
+        }
+        
+        queue.sort((a,b) => b-a);
+        answer.push(queue[queue.length-1]);
+    })
+       
+    
+    return answer;
+}
