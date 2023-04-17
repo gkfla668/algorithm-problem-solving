aa52edc8bf6c95c4bebdf7caa85dca70a6b68b07
Halim-Lim <gkfla668@naver.com>
2023-04-17 20:04:51 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,20 +1,14 @@
 function solution(a, b, n) {
     let answer = 0;
-    let cola = n;
     let tmp = 0;
     
-    while(cola >= a){  // 보유 중인 빈 병이 a개 미만이면, 추가적으로 빈 병을 받을 순 없다.
+    while(n >= a){  // 보유 중인 빈 병이 a개 미만이면, 추가적으로 빈 병을 받을 순 없다.
         tmp = 0;
+        if(n % a !== 0) tmp = n % a
         
-        if(cola % a !== 0) {
-            tmp = cola % a;
-            console.log(`남은 빈병 ${tmp}`);
-        } 
-        
-        cola = Math.floor(cola / a) * b;
-        answer += cola;
-        console.log(`받은 콜라 ${cola}`)
-        cola += tmp;
+        n = Math.floor(n / a) * b;
+        answer += n;
+        n += tmp;
     }
 
     return answer;
