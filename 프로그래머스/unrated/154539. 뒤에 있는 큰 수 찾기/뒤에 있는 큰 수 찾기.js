0700dbf51943ba9a40a7e67036caf07e8e0ed415
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2023-04-19 16:57:28 +0900

    Update README.md

--- 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/unrated/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,36 +1,25 @@
 function solution(numbers) {
-    // return numbers.map((val, idx) => {
-    //     let arr = numbers.slice(idx + 1, numbers.length);
-    //     let num = arr.find((it) => it > val);
-    //     return num ? num : -1;
-    // })
-    
-    let arr = [...numbers];
-    let stack = [arr.pop()];
-    let answer = [];
-    
-    while(arr.length !== 0) {  
-        let num = arr.pop();
-        let backMax = stack[stack.length - 1];
-        
-        if(num < backMax) {
-            answer.push(backMax);
-            stack.push(num);
-        } else {
-            while(num >= stack[stack.length - 1]){
-                stack.pop()
-                continue;
-            }
-            if(stack.length !== 0) {
-                let big = stack[stack.length - 1];
-                answer.push(big);
-            } else answer.push(-1);
-            stack.push(num);
-        }   
+  let stack = [numbers.pop()];
+  let answer = [];
+
+  while (numbers.length !== 0) {
+    let num = numbers.pop();
+    let backMax = stack[stack.length - 1];
+
+    if (num < backMax) answer.push(backMax);
+    else {
+      while (num >= stack[stack.length - 1]) stack.pop();
+
+      if (stack.length !== 0) {
+        backMax = stack[stack.length - 1];
+        answer.push(backMax);
+      } else answer.push(-1);
     }
-    
-    answer.reverse()
-    answer.push(-1)
-    return answer;
-    
+
+    stack.push(num);
+  }
+
+  answer.reverse();
+  answer.push(-1);
+  return answer;
 }
