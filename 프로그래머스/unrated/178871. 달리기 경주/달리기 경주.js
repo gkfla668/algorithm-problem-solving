f9a00adf4999614810485b7eda56bb39ed03f777
Halim-Lim <gkfla668@naver.com>
2023-04-21 20:50:31 +0900

    [unrated] Title: 달리기 경주, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,19 +1,18 @@
 function solution(players, callings) {
-    let map = new Map();
+    let map = new Map(); // 핵심 !
     
     players.forEach((name, idx) => {
-        map.set(name, idx);
+        map.set(name, idx); // 참여자들의 이름을 key로 설정
     })
     
     callings.forEach((name) => {
-        let currIdx = map.get(name);
+        let currIdx = map.get(name); // 핵심 ! O(1)
         let front = players[currIdx - 1]; 
-        
-        
-        [players[currIdx], players[currIdx -1]] = [players[currIdx -1], players[currIdx]];
         
         map.set(name, currIdx - 1);
         map.set(front, map.get(front) + 1);
+      
+      [players[currIdx], players[currIdx -1]] = [players[currIdx -1], players[currIdx]]; // 원본 순서 변경
     })
              
     return players;
