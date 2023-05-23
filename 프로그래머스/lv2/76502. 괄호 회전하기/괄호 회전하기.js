d1a14343a5259f1eb220be33d80af3f3ec509fe6
Halim-Lim <gkfla668@naver.com>
2023-05-23 20:04:28 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.js
@@ -1,25 +1,25 @@
 function solution(s) {
     var answer = 0;
+    let map = new Map();
+    map.set('(', ')');
+    map.set('[', ']');
+    map.set('{', '}');
     
     for(let i = 0; i < s.length; i++){
         let start = [];
-        for(let j = 0; j < s.length; j++){
-            let target = s.charAt(j);
-            if(target === '(' || target === '[' || target === '{'){
-                start.push(s.charAt(j));
-            } else {
+        
+        s = s.slice(1, s.length) + s.slice(0, 1);
+        [...s].forEach((ch) => {
+            let target = ch;
+            if(target === '(' || target === '[' || target === '{')
+                start.push(ch);
+            else {
                 let tmp = start.pop()
-                if(target === ')' && tmp !== '(') start.push(tmp);
-                if(target === ']' && tmp !== '[') start.push(tmp);
-                if(target === '}' && tmp !== '{') start.push(tmp);
+                if(target !== map.get(tmp)) start.push(tmp);
             }
-        }
+        })
         
         if(start.length === 0) answer++;
-        
-        let first = s.substring(0, 1);
-        s = s.substring(1, s.length);
-        s += first;
 
     }
     
