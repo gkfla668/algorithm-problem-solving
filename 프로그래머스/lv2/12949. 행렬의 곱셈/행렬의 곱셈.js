ca81912101987388ab18a07ad09e477f04bef28e
Halim-Lim <gkfla668@naver.com>
2023-06-05 02:00:38 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.35 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -5,31 +5,29 @@ function solution(arr1, arr2) {
     for(let i = 0; i < arr2[0].length; i++){
         let arr = [];
         
-        arr2.forEach((val) => {
-            arr.push(val[i]);
-        }) 
+        arr2.forEach(val => arr.push(val[i])); 
         
-        newArr2.push(arr)
+        newArr2.push(arr);
     }
     
     newArr2 = newArr2.filter((arr) => arr.length !== 0 );
-    
-    console.log(newArr2)
-    
-    
-    arr1.map((a1) => {
-        let arr = [];
         
-        newArr2.forEach((v) => {
+    arr1 = arr1.map((a1) => {
+        let result = [];
+        
+        newArr2.forEach((a2) => {
             let value = 0;
-           for(let i = 0; i < newArr2[0].length; i++){
-                value += a1[i] * v[i];
+            
+            for(let i = 0; i < newArr2[0].length; i++){
+                value += a1[i] * a2[i];
             } 
-            arr.push(value);
+            
+            result.push(value);
         })
         
-        answer.push(arr);
+        return result;
     })
     
-    return answer.filter((a) => a.length !== 0);
+    
+    return arr1;
 }
