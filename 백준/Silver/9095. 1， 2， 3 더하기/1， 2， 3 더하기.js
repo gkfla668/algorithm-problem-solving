ed96f166cdcb4393bfc5b662dc93d98e078d5726
Halim-Lim <gkfla668@naver.com>
2023-06-12 23:42:37 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 124 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -0,0 +1,16 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+let T = input.shift();
+
+let dp = new Array(11).fill(0);
+
+dp[0] = 0;
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 4;
+
+for (let i = 4; i < 11; i++) dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
+for (let i = 0; i < T; i++) {
+  let num = Number(input.shift());
+  console.log(dp[num]);
+}
