48240180012efa7f8858866abe75b18de20cf136
Halim-Lim <gkfla668@naver.com>
2023-06-14 01:29:15 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 760 ms, Memory: 104064 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.js
@@ -0,0 +1,33 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+let [N, M, R] = input.shift().split(" ").map(Number);
+
+let graph = [...Array(N + 1)].map(() => []);
+let visitied = new Array(N).fill(0);
+
+const arr = input.map((v) => v.split(" ").map(Number));
+
+arr.map(([start, end]) => {
+  graph[start].push(end);
+  graph[end].push(start);
+});
+
+graph.map((e) => e.sort((a, b) => a - b));
+
+let cnt = 0;
+const DFS = (start) => {
+  visitied[start - 1] = ++cnt;
+
+  for (let end of graph[start]) {
+    if (visitied[end - 1] === 0) {
+      DFS(end);
+    }
+  }
+};
+
+DFS(R);
+
+console.log(visitied.join("\n"));
