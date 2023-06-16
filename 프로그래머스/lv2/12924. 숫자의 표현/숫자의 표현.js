149df366f4ddb4c7de66b56513eac123f89c805a
Halim-Lim <gkfla668@naver.com>
2023-06-17 04:20:01 +0900

    [level 2] Title: 숫자의 표현, Time: 2.66 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -4,18 +4,9 @@ function solution(n) {
     for(let i = 1; i <= Math.floor(n/2); i++){
         let [next, sum] = [i, 0];
         
-        while(sum < n){
-            sum += next++;  
-            
-            if(sum === n) {
-                answer++;
-                break;
-            }
-        }
-        
-      
-      
+        while(sum < n) sum += next++;  
+        if(sum === n) answer++;
     }
     
-    return answer + 1;
+    return answer + 1; // 자기자신 포함
 }
