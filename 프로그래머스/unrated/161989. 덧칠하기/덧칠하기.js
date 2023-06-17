4e3448147ebf641f6abe9240c1710d3969c0ebbb
Halim-Lim <gkfla668@naver.com>
2023-06-17 18:46:19 +0900

    [unrated] Title: 덧칠하기, Time: 60.94 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -2,10 +2,8 @@ function solution(n, m, section) {
     var answer = 0;
     let arr = new Array(n+1).fill(0);
     
-    section.forEach(v => arr[v] = -1);
-    
     for(let i = 1; i < arr.length; i++){
-        if(arr[i] === -1){
+        if(section.includes(i)){
             answer++;
             i += m - 1;
         }
