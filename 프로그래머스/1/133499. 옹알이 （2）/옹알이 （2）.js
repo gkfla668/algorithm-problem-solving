218184ccb702ce44884207306317f501db5a5e63
Halim-Lim <gkfla668@naver.com>
2023-12-20 16:13:07 +0900

    [level 1] Title: 옹알이 (2), Time: 3.45 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,19 @@
+function solution(babbling) {
+    let answer = 0;
+    let arr = ['aya', 'ye', 'woo', 'ma'];
+    
+    for(let i = 0; i < babbling.length; i++){
+        let str = babbling[i];
+        
+        for(let j = 0; j < arr.length; j++){
+            if(str.includes(arr[j].repeat(2))) break;
+            
+            str = str.replaceAll(arr[j], " ");
+        }
+        
+        console.log(str)
+        if(str.replaceAll(" ", "").length === 0) answer++;
+    }
+    
+    return answer;
+}
