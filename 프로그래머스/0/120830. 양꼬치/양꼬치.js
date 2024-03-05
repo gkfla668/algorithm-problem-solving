dbe5b9bed38c9dfb2285ab4a58ef93954f5cce4b
Halim-Lim <gkfla668@naver.com>
2024-03-05 14:53:00 +0900

    [level 0] Title: 양꼬치, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,8 +1,6 @@
 function solution(n, k) {
-    let answer = n * 12000 + k * 2000
-    
     if(n >= 10)
-        answer -= Math.floor(n/10) * 2000;
+        k -= ~~(n/10);
     
-    return answer;;
+    return n * 12000 + k * 2000;
 }
