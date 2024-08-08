8f99d080f8c88fb2f32007b2bb731a34be5120e1
Halim-Lim <gkfla668@naver.com>
2024-08-08 19:24:35 +0900

    Update 과일_장수.js

--- src/programmers/javascript/level1/과일_장수.js
+++ src/programmers/javascript/level1/과일_장수.js
@@ -1,10 +1,11 @@
 function solution(k, m, score) {
     var answer = 0;
-    score.sort();
-    
-    for(let i = score.length % m; i < score.length; i += m){
-        answer += score[i] * m;
+    score.sort((a, b) => a - b); 
+
+    // 배열의 끝에서부터 m개씩 묶어 처리합니다
+    for (let i = score.length - m; i >= 0; i -= m) {
+        answer += score[i] * m; // 묶음의 가장 작은 점수와 m을 곱하여 answer에 추가
     }
 
-    return answer;
+    return answer; 
 }
