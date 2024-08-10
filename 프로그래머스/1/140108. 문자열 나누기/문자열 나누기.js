4de522f73a810a15b36b9131f84dc333c2c24117
Halim-Lim <gkfla668@naver.com>
2024-08-11 01:52:15 +0900

    [level 1] Title: 문자열 나누기, Time: 25.57 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,26 +1,25 @@
-function solution(s) {
-    let cnt = 0;
-    let newCnt = 0;
-    
+function solution(s) { 
     let answer = 0;
-    
     let target = "";
+    let count1 = 0;
+    let count2 = 0;
     
-
     for(let ch of s){
         if(!target) target = ch;
     
-        ch === target ? cnt++ : newCnt++;
-            
-        if(cnt === newCnt){
-            cnt = 0;
-            newCnt = 0;
+        ch === target ? count1++ : count2++;
+         
+        // 처음으로 두 횟수가 같아지는 순간 멈추고, 지금까지 읽은 문자열을 분리
+        if(count1 === count2){
+            count1 = 0;
+            count2 = 0;
             answer++;
             target = "";
-            }
         }
+    }
     
-     if (target) answer++;
+    // 횟수가 다른 상태에서 더 이상 읽을 글자가 없다면, 역시 지금까지 읽은 문자열을 분리
+    if (target) answer++;
     
     return answer;
 }
