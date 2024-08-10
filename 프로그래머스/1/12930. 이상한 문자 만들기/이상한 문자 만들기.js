71694b03dfd438388407be924ee56f9dd1d22d57
Halim-Lim <gkfla668@naver.com>
2024-08-10 17:25:43 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,14 +1,16 @@
 function solution(s) {
-    s = s.toUpperCase();
-    let words = s.split(' ');
+    s = s.toUpperCase(); // 모두 대문자로 변환
+    let words = s.split(' '); 
     let index = 0;
     
     for(let i = 0; i < words.length; i++){
-        index = 0;
+        index = 0; // 다음 단어 처리, reset
         
+        // 한 단어 처리
         for(let char of words[i]){
-            if(index % 2 !== 0) // 홀수 인덱스 소문자로
+            if(index % 2 !== 0) // 홀수 인덱스인 경우 소문자로 변환
                 words[i] = words[i].slice(0, index) + words[i].charAt(index).toLowerCase() + words[i].slice(index + 1);
+            
             index++;
         }
     }
