c62822288327cceb35774063a7c3017f086fd47a
Halim-Lim <gkfla668@naver.com>
2024-08-11 02:17:55 +0900

    [level 1] Title: 옹알이 (2), Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -6,11 +6,15 @@ function solution(babbling) {
         let str = babbling[i];
         
         for(let j = 0; j < arr.length; j++){
+            // 연속되는 발음 처리
             if(str.includes(arr[j].repeat(2))) break;
             
+            // 연속되는 발음 처리때문에 빈칸으로 변환
             str = str.replaceAll(arr[j], " ");
         }
         
+        // 문자열에서 모든 빈칸을 제거
+        // 길이가 0이라면, 이는 주어진 문자열이 모두 유효한 발음들로만 이루어져 있음을 의미
         if(str.replaceAll(" ", "").length === 0) answer++;
     }
     
