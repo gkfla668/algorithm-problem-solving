6f6f09d6acd7b84672cfaea2fd6f5271de4c3904
임하림 <gkfla668@naver.com>
2024-08-12 20:55:58 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 17.63 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -4,8 +4,9 @@ function solution(s) {
     
     while(s.length !== 1){
         changeCnt++;
-        zeroCnt += (s.match(/0/g) || []).length; // x의 모든 0을 제거합니다.
-        s = s.replaceAll('0', "").length.toString(2); // x를 "x의 길이를 2진법으로 표현한 문자열"로 바꿉니다.
+        // || []는 null.length가 되는 걸 방지하는 코드
+        zeroCnt += (s.match(/0/g) || []).length;
+        s = s.replaceAll('0', "").length.toString(2); // x의 모든 0을 제거하고, x를 "x의 길이를 2진법으로 표현한 문자열"로 바꿉니다.
     }
     
     return [changeCnt, zeroCnt];
