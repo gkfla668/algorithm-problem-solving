58e9c4c008de3c17836f53f37c3609c14e939b32
임하림 <gkfla668@naver.com>
2024-08-12 19:53:45 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,24 @@
 function solution(progresses, speeds) {
     let answer = [];
     let stack = [];
-    let tmp = 0;
     let cnt = 1;
     
+    // 작업 완료까지 걸리는 일수 계산
     for(let i = progresses.length - 1; i >= 0; i--){
         stack.push(Math.ceil((100 - progresses[i]) / speeds[i]))
     }
     
     let target = stack.pop();
     while(stack.length > 0){
-        let tmp = stack.pop();
+        let lastStackItem = stack.pop();
         
-        if(target < tmp){
+        if(target < lastStackItem){
             answer.push(cnt);
-            cnt = 1;
-            target = tmp;
+            cnt = 1; // 초기화
+            target = lastStackItem;
         } else cnt++;
     }
     
     answer.push(cnt);
-    
     return answer;
 }
