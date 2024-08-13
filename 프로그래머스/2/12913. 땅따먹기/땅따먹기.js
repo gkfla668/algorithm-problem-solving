491bc452e75c82499a9530f53aab6d0098c64fa5
임하림 <gkfla668@naver.com>
2024-08-13 23:06:35 +0900

    [level 2] Title: 땅따먹기, Time: 71.87 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,8 +1,10 @@
+// 현재까지의 최적해를 다음 단계로 넘기면서 최종적인 최적해를 구하는 방식으로 문제를 해결하는 DP 기법
 function solution(land) {
-    // 현재 행에서 동일한 열에 위치하지 않은 나머지 열들 중에서 최대값을 선택하여 더하는 것
     land.forEach((row, rowIdx) => {
         row.forEach((col, colIdx) => {
+            // 동일한 열에 위치하지 않는 값들 중에서 최대값을 선택하여 누적 합을 계산
             let arr = row.filter((it, idx) => idx !== colIdx);
+            
             if(rowIdx < land.length - 1){
                 land[rowIdx + 1][colIdx] += Math.max(...arr);
             }
