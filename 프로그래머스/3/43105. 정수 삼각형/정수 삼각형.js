7ec8282a43bf0d7eac165525281118ae8f251f84
임하림 <gkfla668@naver.com>
2024-11-01 15:19:45 +0900

    [level 3] Title: 정수 삼각형, Time: 6.26 ms, Memory: 41.6 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,13 +1,22 @@
 function solution(triangle) {
-    for(let i = 1; i < triangle.length; i++){
+    for(let i = 1; i < triangle.length; i++){ // 삼각형의 두번째 줄부터 시작
         for(let j = 0; j < triangle[i].length; j++){
+            // 현재 위치가 맨 왼쪽인 경우: 위에서 하나만 선택 가능
             if(j === 0) triangle[i][j] += triangle[i - 1][j]; 
+            // 현재 위치가 맨 오른쪽인 경우: 위에서 하나만 선택 가능
             else if(j === triangle[i].length - 1) triangle[i][j] += triangle[i - 1][j - 1]; 
+            // 현재 위치가 가운데인 경우: 위의 두 위치 중 최대 값 선택
             else {
                 triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
             }
         }
     }
     
-    return Math.max(...triangle[triangle.length - 1]);
+      // 마지막 줄에서 최댓값 찾기
+    let maxVal = triangle[triangle.length - 1][0];
+    for (let num of triangle[triangle.length - 1]) {
+        if (num > maxVal) maxVal = num;
+    }
+    
+    return maxVal;
 }
