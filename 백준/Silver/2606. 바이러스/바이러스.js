c1ba0029112a3cec88fa89c7d036726e20428fa3
임하림 <gkfla668@naver.com>
2025-04-10 13:45:31 +0900

    [Silver III] Title: 바이러스, Time: 120 ms, Memory: 9392 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.js
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -1,40 +1,38 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-let N = Number(input[0]);
-let pair = Number(input[1]);
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+var N = parseInt(input[0]);
+var pair = parseInt(input[1]);
 
 let computers = [...new Array(N + 1)].map(() => []);
 let visited = new Array(N + 1).fill(false);
-let cnt = 0;
 
-for (let i = 2; i < pair + 2; i++) {
-  // 2부터 쌍
-  let start = Number(input[i].split(" ")[0]);
-  let end = Number(input[i].split(" ")[1]);
-
-  computers[start].push(end);
-  computers[end].push(start);
+for(let i = 2; i < pair + 2; i++){
+    let start = Number(input[i].split(" ")[0]);
+    let end = Number(input[i].split(" ")[1]);
+    
+    computers[start].push(end);
+    computers[end].push(start);
 }
 
-visited[1] = true; // 1번 컴퓨터가 바이러스에 걸렸으므로 1부터 시작. 1 방문처리.
-
-const BFS = (start) => {
-  let queue = [];
-
-  queue.push(start);
-
-  while (queue.length !== 0) {
-    let node = queue.shift();
-
-    for (let i = 0; i < computers[node].length; i++) {
-      if (!visited[computers[node][i]]) {
-        queue.push(computers[node][i]);
-        visited[computers[node][i]] = true;
-        cnt++;
-      }
+const DFS = (N, start) => {    
+    visited[start] = true;
+    let stack = [];
+    let cnt = 0;
+    
+    stack.push(start);
+    
+    while(stack.length !== 0){
+        let node = stack.shift();
+        for(let i = 1; i <= N; i++){
+            if(computers[node].includes(i) && !visited[i]){
+                stack.push(i);
+                cnt++;
+                visited[i] = true;
+            }
+        }
     }
-  }
-};
+    
+    return cnt;
+}
 
-BFS(1);
-console.log(cnt);
+console.log(DFS(N, 1));
