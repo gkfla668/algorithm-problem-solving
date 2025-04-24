9b087be69da0623f45e361d36b9bc69f4b2c819b
임하림 <gkfla668@naver.com>
2025-04-24 12:55:29 +0900

    [Silver I] Title: 미로 탐색, Time: 176 ms, Memory: 12608 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,49 +1,44 @@
-const dx = [0, 1, 0, -1];
-const dy = [1, 0, -1, 0];
-
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(/\r?\n/);
 
-const size = input.shift().split(" ");
-const N = size[0];
-const M = size[1];
+let [N, M] = input.shift().split(" ").map(Number);
+let arr = Array.from(N); // 미로 배열
 
-const miro = [...new Array(N)].map(() => []);
-for (let i = 0; i < N; i++) {
-  miro[i] = input.shift().trim().split("").map(Number);
+while (input.length > 0) {
+  arr.push(input.shift().split("").map(Number));
 }
 
-const visited = [];
-for (let i = 0; i < N; i++) {
-  visited.push(new Array(M).fill(false));
-}
+// 최소의 칸 수를 구해야 하므로 BFS
 
-let cnt = 0;
+let ix = [1, 0, -1, 0];
+let iy = [0, 1, 0, -1];
+let visited = Array.from({ length: N }, () =>
+  Array.from({ length: M }, () => false)
+);
 
-const BFS = (x, y) => {
+const bfs = (x, y) => {
   let queue = [[x, y]];
-  visited[x][y] = true;
-  miro[x][y] = 1;
+  visited[x][y] = true; // 방문 처리
 
   while (queue.length !== 0) {
-    let [x, y] = queue.shift();
+    let [dx, dy] = queue.shift();
 
+    // 상하좌우 탐색
     for (let i = 0; i < 4; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
+      let [nx, ny] = [dx + ix[i], dy + iy[i]];
 
-      if (rangeCheck(nx, ny) && !visited[nx][ny] && miro[nx][ny] === 1) {
-        visited[nx][ny] = true;
-        miro[nx][ny] = miro[x][y] + 1;
-        queue.push([nx, ny]);
+      // 좌표가 유효범위인지
+      if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+        // 이동할 수 있는 칸이며 아직 방문하지 않았을 경우
+        if (arr[nx][ny] === 1 && !visited[nx][ny]) {
+          arr[nx][ny] = arr[dx][dy] + 1; // 칸 수 누적
+          visited[nx][ny] = true; // 방문 처리
+          queue.push([nx, ny]);
+        }
       }
     }
   }
 };
 
-const rangeCheck = (x, y) => {
-  return x >= 0 && x < N && y >= 0 && y < M;
-};
-
-BFS(0, 0);
-console.log(miro[N - 1][M - 1]);
+bfs(0, 0);
+console.log(arr[N - 1][M - 1]);
